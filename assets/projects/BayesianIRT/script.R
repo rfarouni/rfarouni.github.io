14c45711620f035954f6fb9afe6203ea4d046528
Rick Farouni <rfarouni@gmail.com>
2015-03-15 00:58:11 -0400

    Update

--- assets/projects/BayesianIRT/script.R
+++ assets/projects/BayesianIRT/script.R
@@ -4,8 +4,7 @@ library(shinyapps)
 library(shinyStan)
 library(rstan)
 load(".RData")
-shinyapps::setAccountInfo(name='rfarouni', token='57EC0707EF56DF97CF3FA5952737E441', 
-                          secret='6n5CJwh7gqwkV762/2KS0FMf6u12aqsr7eLaVqfj')
+
 shinystan_object <-as.shinystan(model.fit)
 save(shinystan_object, file = "shinystan_object.RData")
 
